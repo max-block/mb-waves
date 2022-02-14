0f61b9fb4c87c855b2cd88e89b124c3490aeade4
Max Block <maxblock@pm.me>
2022-02-14 13:29:36 +0300

    update

--- mb_waves/waves/node_api.py
+++ mb_waves/waves/node_api.py
@@ -27,6 +27,21 @@ def asset_balance(node: str, address: str, asset_id: str, timeout=10, proxy=None
         return res.to_error(f"exception: {str(e)}")
 
 
+def tx_info(node: str, tx_id: str, timeout=10, proxy=None) -> Result[dict]:
+    url = f"{_add_slash(node)}transactions/info/{tx_id}"
+    res = hr(url, timeout=timeout, proxy=proxy)
+    try:
+        if res.is_error():
+            return res.to_error()
+        if res.json == {"error": 311, "message": "transactions does not exist"}:
+            return res.to_error("tx_not_found")
+        if "type" in res.json and "id" in res.json:
+            return res.to_ok(res.json)
+        return res.to_error("unknown_response")
+    except Exception as e:
+        return res.to_error(f"exception: {str(e)}")
+
+
 def _add_slash(node: str) -> str:
     if node.endswith("/"):
         return node
